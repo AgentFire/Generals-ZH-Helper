d0818e89fff9b5c7fd8ea2cf9685525b46b8fea7
AgentFire <agentfire@mail.ru>
2020-12-30 01:37:56 +0300

    first decade, manifest

--- MainLoop/Services/ProcessDetector.cs
+++ MainLoop/Services/ProcessDetector.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,30 +10,37 @@ namespace GenHelper.MainLoop.Services
     public sealed class ProcessDetector : IDisposable
     {
         private const string ProcessName = "generals";
+        private static readonly IReadOnlyList<(string Path, string Key)> RegistryPaths = new List<(string Path, string Key)> {
+            (@"SOFTWARE\Electronic Arts\EA Games\Command and Conquer Generals Zero Hour", "InstallPath"),
+            (@"SOFTWARE\Electronic Arts\EA Games\Command and Conquer The First Decade", "InstallFolder")
+        };
 
-        private static Func<Process, bool> BuildProcessCorrectnessPredicate()
+        private static readonly RegistryKey _view32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+
+        private static Func<Process, bool> ProcessCorrectnessPredicate { get; } = T =>
         {
-            try
+            foreach (var regPair in RegistryPaths)
             {
-                using var view32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
-                using var key = view32.OpenSubKey(@"SOFTWARE\Electronic Arts\EA Games\Command and Conquer Generals Zero Hour", false);
-
-                string installPath = (string)key.GetValue("InstallPath");
-                string normalizedInstallPath = PathHelpers.NormalizePath(installPath);
-
-                return T =>
+                try
                 {
+                    using var key = _view32.OpenSubKey(regPair.Path, false);
+
+                    string installPath = (string)key.GetValue(regPair.Key);
+                    string normalizedInstallPath = PathHelpers.NormalizePath(installPath);
+
                     string processPath = Path.GetDirectoryName(T.MainModule.FileName);
                     string normalizedProcessPath = PathHelpers.NormalizePath(processPath);
 
-                    return normalizedProcessPath == normalizedInstallPath;
-                };
+                    if (normalizedProcessPath == normalizedInstallPath)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception ex) { }
             }
-            catch
-            {
-                return _ => false;
-            }
-        }
+
+            return false;
+        };
 
         private Process? _gameProcess = null;
 
@@ -47,7 +55,7 @@ namespace GenHelper.MainLoop.Services
             {
                 try
                 {
-                    _gameProcess = Process.GetProcessesByName(ProcessName).Where(BuildProcessCorrectnessPredicate()).OrderByDescending(T => T.PrivateMemorySize64).FirstOrDefault();
+                    _gameProcess = Process.GetProcessesByName(ProcessName).Where(ProcessCorrectnessPredicate).OrderByDescending(T => T.PrivateMemorySize64).FirstOrDefault();
                 }
                 catch
                 {
